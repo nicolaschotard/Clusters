b80ea1ba4f1f81b3de8634d49521349f319bd124
Nicoals Chotard <nchotard@in2p3.fr>
2016-08-10 13:42:00 +0200

    PEP8

--- lib/data.py
+++ lib/data.py
@@ -5,11 +5,12 @@ from astropy.table import Table, Column, vstack
 def load_config(config):
     """Load the configuration file, and return the corresponding dictionnary
 
+
     :param config: Name of the configuretion file.
     :type config: str.
     :returns: dic
 
-    """ 
+    """
     return yaml.load(open(config))
 
 def shorten(doc):
@@ -17,19 +18,19 @@ def shorten(doc):
     Really bad hack to go around an astropy/hdf5 bug
     Cut in half words longer than 18 character
     """
-    return " ".join([w if len(w) < 18 else (w[:len(w)/2]+' - '+w[len(w)/2:]) for w in doc.split()])
+    return " ".join([w if len(w) < 18 else (w[:len(w)/2] + ' - ' + w[len(w)/2:]) for w in doc.split()])
 
 
 def get_astropy_table(cat):
     """
     Convert an afw data table into a simple astropy table
     """
-    schema = cat.getSchema() 
+    schema = cat.getSchema()
     dic = {n: cat.get(n) for n in schema.getNames()}
     tab = Table(dic)
     for k in schema.getNames():
-        tab[k].description=shorten(schema[k].asField().getDoc())
-        tab[k].unit=schema[k].asField().getUnits()
+        tab[k].description = shorten(schema[k].asField().getDoc())
+        tab[k].unit = schema[k].asField().getUnits()
     return tab
 
 def get_from_butler(butler, key, filt, patch, tract=0, table=False):
@@ -37,19 +38,19 @@ def get_from_butler(butler, key, filt, patch, tract=0, table=False):
     Return selected data from a butler for a given key, tract, patch and filter
     Either retrun the object or the astropy table version of it
     """
-    dataId = {'tract': tract, 'filter': filt, 'patch': patch}
-    b = butler.get(key, dataId=dataId)
+    dataid = {'tract': tract, 'filter': filt, 'patch': patch}
+    b = butler.get(key, dataId=dataid)
     return b if not table else get_astropy_table(b)
 
-def add_magnitudes(t, getMagnitude):
+def add_magnitudes(t, getmagnitude):
     """
     Compute magnitude for all fluxes of a given table and add the corresponding
     new columns
     """
-    Kfluxes = [k for k in t.columns if k.endswith('_flux')]
-    Ksigmas = [k+'Sigma' for k in Kfluxes]
-    for kf, ks in zip(Kfluxes, Ksigmas):
-        m, dm = N.array([getMagnitude(f, s) for f, s in zip(t[kf], t[ks])]).T
+    kfluxes = [k for k in t.columns if k.endswith('_flux')]
+    ksigmas = [k + 'Sigma' for k in kfluxes]
+    for kf, ks in zip(kfluxes, ksigmas):
+        m, dm = N.array([getmagnitude(f, s) for f, s in zip(t[kf], t[ks])]).T
         t.add_columns([Column(name=kf.replace('_flux', '_mag'), data=m,
                               description='Magnitude', unit='mag'),
                        Column(name=ks.replace('_fluxSigma', '_magSigma'), data=dm,
@@ -57,7 +58,7 @@ def add_magnitudes(t, getMagnitude):
 
 def add_position(t, wcs_alt):
     """
-    Compute the x/y position in pixel for all sources and add new columns to 
+    Compute the x/y position in pixel for all sources and add new columns to
     the astropy table
     """
     x, y = N.array([wcs_alt(ra, dec) for ra, dec in zip(t["coord_ra"], t["coord_dec"])]).T
@@ -67,10 +68,10 @@ def add_position(t, wcs_alt):
                           description='y coordinate', unit='pixel')])
 
 def add_filter_column(t, f):
-    t.add_column(Column(name='filter', data=[f]*len(t), description='Filter name'))
+    t.add_column(Column(name='filter', data=[f] * len(t), description='Filter name'))
 
 def add_patch_column(t, p):
-    t.add_column(Column(name='patch', data=[p]*len(t), description='Patch name'))
+    t.add_column(Column(name='patch', data=[p] * len(t), description='Patch name'))
 
 def add_extra_info(d):
     """
@@ -85,8 +86,9 @@ def add_extra_info(d):
 
     # redifine wcs
     import lsst.afw.geom as afwGeom
-    wcs_alt = lambda r, d: wcs.skyToPixel(afwGeom.geomLib.Angle(r), afwGeom.geomLib.Angle(d))
-    
+    def wcs_alt(r, d):
+        return wcs.skyToPixel(afwGeom.geomLib.Angle(r), afwGeom.geomLib.Angle(d))
+
     getmag = d[f][p]['calexp'].getCalib().getMagnitude
 
     # redefine the magnitude function to make it 'work' for negative flux or sigma
@@ -135,8 +137,8 @@ def get_patch_data(butler, p, f):
     meas = get_from_butler(butler, 'deepCoadd_meas', f, p, table=True)
     forced = get_from_butler(butler, 'deepCoadd_forced_src', f, p, table=True)
     calexp = get_from_butler(butler, 'deepCoadd_calexp', f, p, table=False)
-    return {'meas': meas, 'forced': forced, 'calexp':calexp}
-    
+    return {'meas': meas, 'forced': forced, 'calexp': calexp}
+
 def from_list_to_array(d):
     """
     Transform lists (of dict of list) into numpy arrays
@@ -183,8 +185,8 @@ def filter_table(t):
     nfilt = len(t['meas'].group_by('id').groups[0])
 
     # Select galaxies (and reject stars)
-    filt = t['meas']['base_ClassificationExtendedness_flag'] == 0 # keep galaxy
-    filt &= t['meas']['base_ClassificationExtendedness_value'] >= 0.5 # keep galaxy
+    filt = t['meas']['base_ClassificationExtendedness_flag'] == 0  # keep galaxy
+    filt &= t['meas']['base_ClassificationExtendedness_value'] >= 0.5  # keep galaxy
 
     # Gauss regulerarization flag
     filt &= t['meas']['ext_shapeHSM_HsmShapeRegauss_flag'] == 0
@@ -194,14 +196,14 @@ def filter_table(t):
 
     # Check the flux value, which must be > 0
     filt &= t['forced']['modelfit_CModel_flux'] > 0
-    
+
     #Select sources which have a proper flux value
     filt &= t['forced']['modelfit_CModel_flag'] == 0
-    
+
     # Check the signal to noise (stn) value, which must be > 10
-    filt &= (t['forced']['modelfit_CModel_flux'] / \
-              t['forced']['modelfit_CModel_fluxSigma']) > 10
-    
+    filt &= (t['forced']['modelfit_CModel_flux'] / 
+             t['forced']['modelfit_CModel_fluxSigma']) > 10
+
     # Only keeps sources with the 5 filters
     dmg = t['meas'][filt].group_by('id')
     dfg = t['forced'][filt].group_by('objectId')
