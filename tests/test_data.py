51bb307dbb12eb1ebb820112e3c291f98d708131
Nicoals Chotard <nchotard@in2p3.fr>
2016-08-24 10:19:59 +0200

    Add first test. Cache pip installs in travis.

---
+++ tests/test_data.py
@@ -0,0 +1,12 @@
+"""Test the reddening module."""
+
+from Clusters import data
+
+
+CONFIG = "../configs/MACSJ2243.3-0935.yaml"
+
+# Test the data module
+
+def test_load_config():
+    """Try to load a real config (yaml) file."""
+    data.load_config(CONFIG)
