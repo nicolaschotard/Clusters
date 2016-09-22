e8f5aaccf0babe57951660bb126f3ba221defe82
Nicoals Chotard <nchotard@in2p3.fr>
2016-09-22 11:30:15 +0200

    Remove nbsphinx from the extension list

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,8 +34,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.coverage',
-    'nbsphinx'
+    'sphinx.ext.coverage'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
