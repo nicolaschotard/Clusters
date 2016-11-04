5be886948f1e30dfa28405068863d930a20a060e
Nicoals Chotard <nchotard@in2p3.fr>
2016-11-04 15:38:53 +0100

    Add shape error on mass file

--- clusters/mass.py
+++ clusters/mass.py
@@ -6,15 +6,16 @@ from . import shear
 from astropy.table import Column
 
 
-def build_table():
-    config = data.load_config('MACSJ2243.3-0935.yaml')
-    catalogs = data.read_hdf5('MACSJ2243.3-0935_filtered_data.hdf5')
+def build_table(config='MACSJ2243.3-0935.yaml', datafile='MACSJ2243.3-0935_filtered_data.hdf5'):
+    config = data.load_config(config)
+    catalogs = data.read_hdf5(datafile)
     table = catalogs['deepCoadd_meas']
     xclust, yclust = shear.xy_clust(config, data.load_wcs(catalogs['wcs']))
     e1r = table["ext_shapeHSM_HsmShapeRegauss_e1"][table['filter'] == 'r']
     e2r = table["ext_shapeHSM_HsmShapeRegauss_e2"][table['filter'] == 'r']
     e1i = table["ext_shapeHSM_HsmShapeRegauss_e1"][table['filter'] == 'i']
     e2i = table["ext_shapeHSM_HsmShapeRegauss_e2"][table['filter'] == 'i']
+    sigmai = table["ext_shapeHSM_HsmShapeRegauss_sigma"][table['filter'] == 'i']
     distx = table["x_Src"][table['filter'] == 'r'] - xclust
     disty = table["y_Src"][table['filter'] == 'r'] - yclust
     objectid = table['id'][table['filter'] == 'i']
@@ -30,7 +31,8 @@ def build_table():
     filt &= (abs(e1r - e1i) < 0.5) & (abs(e2r - e2i) < 0.5)
 
     # Apply cuts
-    e1i, e2i, distx, disty, objectid = [x[filt] for x in [e1i, e2i, distx, disty, objectid]]
+    e1i, e2i, distx, disty, objectid, sigmai = [x[filt] for x in [e1i, e2i, distx,
+                                                                  disty, objectid, sigmai]]
 
     # Comput the shear
     gamt, gamc, dist = shear.compute_shear(e1i, e2i, distx, disty)
@@ -40,5 +42,9 @@ def build_table():
 
     filt = [i for i, oid in enumerate(params['objectId']) if oid in objectid]
     params = params[filt]
-    params.add_columns([Column(data=gamt, name='Tshear'), Column(data=gamc, name='Cshear')])
+    params.add_columns([Column(data=gamt, name='Tshear'),
+                        Column(data=gamc, name='Cshear'),
+                        Column(data=e1i, name='Ell-1-ir'),
+                        Column(data=e2i, name='Ell-2-i'),
+                        Column(data=sigmai, name='Sigma-ell'),])
     params.write("data_yves.txt", format='ascii')
