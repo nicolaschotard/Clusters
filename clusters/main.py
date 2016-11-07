f4ef7ce081d744c5e21f007591c057b33b0fa849
Nicoals Chotard <nchotard@in2p3.fr>
2016-11-07 14:06:45 -0500

    Make a clean list of filters for lephare input

--- clusters/main.py
+++ clusters/main.py
@@ -206,14 +206,14 @@ def photometric_redshift(argv=None):
     for i, zpara in enumerate(args.zpara.split(',') if isinstance(args.zpara, str) else args.zpara):
         print "\nINFO: Configuration for LEPHARE from:", zpara
         kwargs = {'basename': config['cluster'] + '_' + zpara.split('/')[-1].replace('.para', ''),
-                  'filters': config['filter'],
+                  'filters': [f for f in config['filters'] if f in set(data['filter'].tolist())],
                   'ra': data['coord_ra_deg'][data['filter'] == config['filter'][0]],
                   'dec': data['coord_dec_deg'][data['filter'] == config['filter'][0]],
                   'id': data['objectId'][data['filter'] == config['filter'][0]]}
-        zphot = czphot.LEPHARE([data[args.mag][data['filter'] == f] for f in config['filter']],
+        zphot = czphot.LEPHARE([data[args.mag][data['filter'] == f] for f in kwargs['filters']],
                                [data[args.mag.replace("_extcorr", "") +
                                      "Sigma"][data['filter'] == f]
-                                for f in config['filter']],
+                                for f in kwargs['filters']],
                                zpara=zpara, spectro_file=spectro_file, **kwargs)
         zphot.check_config()
         zphot.run()
