972e6ef9917fb14e44f807d30f21503bffc70799
Douglas Applegate <dapple@Douglass-MacBook-Air.local>
2016-11-07 13:36:21 -0500

    successful compile of cython files

--- setup.py
+++ setup.py
@@ -6,7 +6,9 @@ import os
 import glob
 import yaml
 
-from setuptools import setup
+from setuptools import setup, find_packages, Extension
+from Cython.Build import cythonize
+import numpy
 
 
 README = '/'.join(os.path.realpath(__file__).split('/')[:-1]) + '/README.rst'
@@ -18,13 +20,21 @@ __version__ = yaml.load(open(VERSION))['version']
 # Package name
 name = 'Clusters'
 
-# Packages (subdirectories in clusters/)
-packages = ["Clusters"]
-
 # Scripts (in scripts/)
 scripts = glob.glob("scripts/*.py")
 
 package_data = {}
+
+extensions = [
+    Extension("pzmassfitter.nfwmodeltools", ["pzmassfitter/nfwmodeltools.pyx"],
+              include_dirs = [numpy.get_include()]
+              ),
+    Extension("pzmassfitter.voigtcall",
+              ["pzmassfitter/voigtcall.pyx", "pzmassfitter/voigt.c"],
+              include_dirs = [numpy.get_include()]
+              ),
+    ]
+
 
 setup(name=name,
       version=__version__,
@@ -33,13 +43,16 @@ setup(name=name,
       classifiers=["Topic :: Scientific :: Astronomy",
                    "Intended Audience :: Science/Research"],
       url="https://github.com/nicolaschotard/Clusters",
-      author="Nicolas Chotard, Dominique Boutigny, Celine Combet",
+      author="Nicolas Chotard, Dominique Boutigny, Celine Combet, Douglas Applegate",
       author_email="nchotard@in2p3.fr",
       package_dir={name: 'clusters'},
-      packages=packages,
+      packages = find_packages(),
+      ext_modules = cythonize(extensions),
       scripts=scripts,
       package_data=package_data,
       long_description=open(README).read(),
       setup_requires=['pytest-runner'],
       tests_require=['pytest']
 )
+
+
