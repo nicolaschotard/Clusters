16b4df2cf7dba3f26de9c37f5103186b69e7d0cc
Nicoals Chotard <nchotard@in2p3.fr>
2016-11-30 11:01:54 +0100

    Cleaning - Bug corrected

--- pzmassfitter/dmstackdriver.py
+++ pzmassfitter/dmstackdriver.py
@@ -11,15 +11,15 @@ import astropytable_filehandler as atf
 
 #######################
 
-make_testing_controller = lambda: maxlike_controller.Controller(modelbuilder=maxlike_masses.LensingModel(),
-                                                                filehandler=atf.AstropyTableFilehandler(),
-                                                                runmethod=maxlike_masses.ScanModelToFile())
+makeTestingController = lambda: maxlike_controller.Controller(modelbuilder=maxlike_masses.LensingModel(),
+                                                              filehandler=atf.AstropyTableFilehandler(),
+                                                              runmethod=maxlike_masses.ScanModelToFile())
 
-make_controller = lambda: maxlike_controller.Controller(modelbuilder=maxlike_bentstep_voigt.BentVoigtShapedistro(),
-                                                        filehandler=atf.AstropyTableFilehandler(),
-                                                        runmethod=maxlike_masses.SampleModelToFile())
+makeController = lambda: maxlike_controller.Controller(modelbuilder=maxlike_bentstep_voigt.BentVoigtShapedistro(),
+                                                       filehandler=atf.AstropyTableFilehandler(),
+                                                       runmethod=maxlike_masses.SampleModelToFile())
 
-controller = make_controller()
+controller = makeController()
 
 
 if __name__ == '__main__':
